da2ba9d83dafdffc4b1c806c22523a709ba465f0
levanthuan2001qttl <81482705+levanthuan2001qttl@users.noreply.github.com>
2022-03-06 21:29:41 +0700

    ket noi DB thanh cong, tien hanh trien khai len heroku

--- server/index.js
+++ server/index.js
@@ -1,7 +1,24 @@
+const { log } = require("console");
 const express = require("express");
-const PORT = process.env.PORT || 5001;
+const mongoose = require('mongoose')
 const app = express();
 const path = require("path");
+require('dotenv').config()
+const PORT = process.env.PORT || 8000;
+const User = require("./app/models/User");
+
+
+
+//  connect DB
+mongoose.connect(process.env.MONGODB_URI)
+mongoose.connection.on('connected', () => {
+    console.log('Connected to Database')
+})
+mongoose.connection.on('error', (err) => {
+  console.log('Connecting error', err)
+})
+
+
 
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, "../client/build")));
