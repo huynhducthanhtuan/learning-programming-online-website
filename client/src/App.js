6aef67c3272f38513e78eda8934502da130ce5cf
levanthuan2001qttl <81482705+levanthuan2001qttl@users.noreply.github.com>
2022-03-16 23:47:50 +0700

    upload

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Home from "./Components/Home/Home";
 import GlobalStyle from "./Components/GlobalStyle";
+import Signup from './Components/Signup'
 function App() {
   return (
     <GlobalStyle>
         <Routes>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/signup" element={<Signup />}></Route>
         </Routes>
     </GlobalStyle>
   )
