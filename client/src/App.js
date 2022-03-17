f5fd079295a0a5215ea490f1011915b13f7f89ad
huynhdthanhtuan <tuan40655@gmail.com>
2022-03-18 00:49:49 +0700

    (FE) Cap nhat logic cho file Header.js, Auth/index.js..........

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,10 @@
 import React, { useReducer, useContext, createContext } from "react";
 import { Routes, Route } from "react-router-dom";
-import "./App.css";
 import { reducer, initialState } from "./reducers";
-// import ForgotPassword from "./components/Forgot password"
-// import EnterCode from "./components/Forgot Password-Enter code"
-// import ForgotRePass from "./components/Forgot Password-Re Pass"
-// import SignIn from "./components/Sign in"
-// import SignUp from "./components/Signup";
 import SignUp from "./Components/Signup";
 import Signin from "./Components/SignIn";
 import Home from "./Components/Home";
+import "./App.css";
 
 export const UserContext = createContext();
 
