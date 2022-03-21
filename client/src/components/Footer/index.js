781b553dc8c1e3490b8c9255b0dd6f25e0204d0a
Kurosaki Aki <kurosakiaki890@gmail.com>
2022-03-21 10:28:41 +0700

    fix footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import styles from "./Footer.module.css";
+import styles from "./footer.module.css";
 
 const Footer = () => {
   return (
