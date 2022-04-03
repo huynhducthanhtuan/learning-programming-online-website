475638b15166b5de082eabd46f2552d3abf3e1bd
Kurosaki Aki <kurosakiaki890@gmail.com>
2022-04-03 09:45:45 +0700

    hieuga

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect}from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Header from "../Header";
 import Slider from "react-slick";
@@ -7,20 +7,11 @@ import CardCourse from "../CardCourse";
 import { getCourses } from "./apiCore";
 import ResultSearch from './ResultSearch'
 import styles from "./Home.module.css";
-import { isAuth,isAuthenticated } from "../Auth";
+import { isAuth, isAuthenticated } from "../Auth";
 import "./HomeSlick.css";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import { addItem } from "../Cart/helperCart";
 
 const Home = () => {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-  };
 
   const [courseBySell, setCourseBySell] = useState([]);
   const [courseByArrival, setCourseByArrival] = useState([]);
@@ -48,7 +39,7 @@ const Home = () => {
 
   const addToCart = (courseToAdd) => {
     addItem(courseToAdd, () => {
-        setRedirect(true)
+      setRedirect(true)
     })
   }
 
@@ -73,11 +64,38 @@ const Home = () => {
           </p>
         </div>
       </section>
-    {/* {ResultSearch()} */}
+      {/* {ResultSearch()} */}
       <section className={`container ${styles.homeSlider}`}>
+
         <h2>New arrivals</h2>
-        <Slider {...settings}>
+        <div className="d-flex justify-content-between">
           {courseByArrival.map((course, i) => (
+            <div key={i} className={`row ${styles.listCourse}`}>
+                <article className={`col ${styles.listCourse__item}`}>
+                  <Link to={`/course/${course._id}`}>
+                    <img className={styles.itemImage} src={course.image} alt="" />
+                    <h6 className="mt-2">{course.name}</h6>
+                  </Link>
+                  <div className={styles.itemCourseText}>
+                    <span>vo trung hieu</span>
+                    <div className={styles.listCourseItemStar}>
+                      <p>(295,007)</p>
+                    </div>
+                    <span className={styles.money}>${course.price}</span>
+                  </div>
+                  <button className='btn btn-outline-info mt-2 mb-2' onClick={() => addToCart(course)}>
+                    Add to cart
+                  </button>
+
+                </article>
+            </div>
+          ))}
+        </div>
+      </section>
+      <section className={`container ${styles.homeSlider}`}>
+        <h2>Best Sellers</h2>
+        <div className={styles.homeListItem}>
+          {courseBySell.map((course, i) => (
             <div key={i} className={styles.listCourse}>
               <div className="row">
                 <article className={`col listCourse__item`}>
@@ -85,50 +103,20 @@ const Home = () => {
                     <img className={styles.itemImage} src={course.image} alt="" />
                     <h6 className="mt-2">{course.name}</h6>
                   </Link>
-                    <div className={styles.itemCourseText}>
-                      <span>vo trung hieu</span>
-                      <div className={styles.listCourseItemStar}>
-                        <p>(295,007)</p>
-                      </div>
-                      <span className={styles.money}>${course.price}</span>
+                  <div className={styles.itemCourseText}>
+                    <span>vo trung hieu</span>
+                    <div className={styles.listCourseItemStar}>
+                      <p>(295,007)</p>
                     </div>
-                    <button className='btn btn-outline-info mt-2 mb-2' onClick={() => addToCart(course)}>
-                        Add to cart
-                    </button>
-
+                    <span className={styles.money}>${course.price}</span>
+                  </div>
+                  <button className='btn btn-outline-info mt-2 mb-2' onClick={() => addToCart(course)}>
+                    Add to cart
+                  </button>
                 </article>
               </div>
             </div>
           ))}
-        </Slider>
-      </section>
-      <section className={`container ${styles.homeSlider}`}>
-        <h2>Best Sellers</h2>
-        <div className={styles.homeListItem}>
-          <Slider {...settings}>
-            {courseBySell.map((course, i) => (
-              <div key={i} className={styles.listCourse}>
-                <div className="row">
-                  <article className={`col listCourse__item`}>
-                  <Link to={`/course/${course._id}`}>
-                    <img className={styles.itemImage} src={course.image} alt="" />
-                    <h6 className="mt-2">{course.name}</h6>
-                  </Link>
-                    <div className={styles.itemCourseText}>
-                      <span>vo trung hieu</span>
-                      <div className={styles.listCourseItemStar}>
-                        <p>(295,007)</p>
-                      </div>
-                      <span className={styles.money}>${course.price}</span>
-                    </div>
-                    <button className='btn btn-outline-info mt-2 mb-2' onClick={() => addToCart(course)}>
-                        Add to cart
-                    </button>
-                  </article>
-                </div>
-              </div>
-            ))}
-          </Slider>
         </div>
       </section>
 
