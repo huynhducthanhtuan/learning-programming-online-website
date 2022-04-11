d3a001b70d6d9c8554bd7cb101232ac7cb26640e
huynhdthanhtuan <tuan40655@gmail.com>
2022-04-12 01:39:49 +0700

    delete all

--- client/src/components/CourseDetail/apiCourseDetail.js
+++
@@ -1,9 +0,0 @@
-export const readLesson = (lessonId) => {
-    return (
-        fetch(`/lesson/${lessonId}`, {
-            method: "GET"
-        })
-        .then(res => res.json())
-        .catch(err => console.log(err))
-    )
-}
