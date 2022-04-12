f52274b982406305e854755eb1b9e97676ff6690
huynhdthanhtuan <tuan40655@gmail.com>
2022-04-12 20:20:41 +0700

    release 2

--- server/app/controllers/auth.js
+++ server/app/controllers/auth.js
@@ -1,76 +1,166 @@
-const User = require("../models/User");
-const jwt = require("jsonwebtoken");
-const { errorHandler } = require("../helpers/dbErrorHandler");
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import styles from "./SignUp.module.css";
 
-exports.signup = (req, res) => {
-  const user = new User(req.body);
-
-  user.save((err, user) => {
-    if (err) {
-      return res.status(400).json({
-        err: errorHandler(err),
-      });
-    }
-
-    user.salt = undefined;
-    user.hashed_password = undefined;
-
-    res.json({
-      user,
-    });
+const SignUp = () => {
+  const [values, setValues] = useState({
+    name: "",
+    email: "",
+    password: "",
+    role: 0,
+    error: "",
+    success: false,
   });
-};
+  const { name, email, password, role, error, success } = values;
 
-exports.signin = (req, res, next) => {
-  const { email, password } = req.body;
-
-  User.findOne({ email }, (err, user) => {
-    if (err || !user) {
-      return res.status(400).json({
-        error: "Account not found. Please signup.",
-      });
+  const handleChange = (name) => (event) => {
+    if (name == "role") {
+      setValues({ ...values, [name]: Number(event.target.value) });
     } else {
-      // if user is found make sure email and password match
-      // use authenticate method in user model
-      if (!user.authenticate(password)) {
-        return res.status(401).json({
-          error: "Email and password doesn't match.",
-        });
+      setValues({ ...values, [name]: event.target.value });
+    }
+  };
+
+  const signUpApi = (user) => {
+    return fetch("/auth/signup", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(user),
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .catch((err) => {
+        return err;
+      });
+  };
+
+  const submitForm = (e) => {
+    e.preventDefault();
+
+    signUpApi({ name, email, password, role }).then((data) => {
+      if (data.error || data.err) {
+        setValues({ ...values, error: data.error || data.err, success: false });
+      } else {
+        setValues({ ...values, error: "", success: true });
       }
+    });
+  };
 
-      //token
-      const token = jwt.sign({ _id: user.id }, process.env.JWT_SECRET);
+  const errorShow = () => {
+    return (
+      <div
+        className="alert alert-danger"
+        style={{ display: error ? "" : "none" }}
+      >
+        {error}
+      </div>
+    );
+  };
 
-      // cookie expire
-      res.cookie("t", token, { expire: new Date() + 9999 });
+  const successShow = () => {
+    return (
+      <div
+        className="alert alert-info"
+        style={{ display: success ? "" : "none" }}
+      >
+        Sign up success. <Link to="/signin">Sign In</Link>
+      </div>
+    );
+  };
 
-      // return response with user and token to client
-      const { _id, role } = user;
+  const signupForm = () => {
+    return (
+      <form className={styles.form} id="form-1">
+        {errorShow()}
+        {successShow()}
+        <div className={styles.formGroup}>
+          <div className={styles.flex}>
+            <p className={styles.formLabel}>Name</p>
+            <p className={styles.formForce}>*</p>
+          </div>
 
-      return res.json({
-        token: token,
-        user: {
-          _id,
-          role,
-        },
-      });
-    }
-  });
+          <input
+            className={styles.formControl}
+            type="text"
+            onChange={handleChange("name")}
+          />
+        </div>
+
+        <div className={styles.formGroup}>
+          <div className={styles.flex}>
+            <p className={styles.formLabel}>Email</p>
+            <p className={styles.formForce}>*</p>
+          </div>
+
+          <input
+            className={styles.formControl}
+            type="email"
+            onChange={handleChange("email")}
+          />
+        </div>
+
+        <div className={styles.formGroup}>
+          <div className={styles.flex}>
+            <p className={styles.formLabel}>Password</p>
+            <p className={styles.formForce}>*</p>
+          </div>
+
+          <input
+            className={styles.formControl}
+            type="password"
+            onChange={handleChange("password")}
+          />
+        </div>
+
+        <div className={styles.formGroup}>
+          <div className={styles.flex}>
+            <p className={styles.formLabel}>Role</p>
+            <p className={styles.formForce}>*</p>
+          </div>
+          <select
+            className={styles.formControl}
+            onChange={handleChange("role")}
+          >
+            <option key={0} value={0}>
+              Student
+            </option>
+            <option key={1} value={1}>
+              Teacher
+            </option>
+          </select>
+        </div>
+        <button
+          type="button"
+          className={`${styles.formSubmit}`}
+          onClick={(e) => submitForm(e)}
+        >
+          Sign up
+        </button>
+      </form>
+    );
+  };
+
+  return (
+    <section className={`mt-4 ${styles.flex}`}>
+      <div>
+        <h2 className={styles.textSignUp}>Sign Up</h2>
+        <div className="mt-4">{signupForm()}</div>
+      </div>
+      <div>
+        <img
+          src="./images/sign-in.png"
+          className={styles.imageSignUp}
+          alt=""
+        ></img>
+        <Link to="/signin">
+          <p className={styles.link}>Already have account? Sign in</p>
+        </Link>
+      </div>
+    </section>
+  );
 };
 
-exports.signout = (req, res, next) => {
-  res.clearCookie("t");
-  res.json({ message: "Sign out success !!" });
-};
-
-exports.isAuthenticated = (req, res, next) => {
-  if (typeof window == "undefined") {
-    return false;
-  } else {
-    if (localStorage.getItem("jwt")) {
-      return JSON.parse(localStorage.getItem("jwt"));
-    } else {
-      return false;
-    }
-  }
-};
+export default SignUp;
